a60462cb6f6a9fcd9f001d18cb7796d94b5de2d5
James <apisitamornktn@gmail.com>
2024-07-10 14:42:25 +0700

    feat:Two Pointer

--- ex-1.js
+++ ex-1.js
@@ -1,3 +1,20 @@
-function twoSum(numbers, target) {
-	// เริ่มเขียนโค้ดตรงนี้จ้า
-};
+function twoSum(numbers, result) {
+  let left = 0;
+  let right = numbers.length - 1;
+
+  while (left < right) {
+    let sum = numbers[left] + numbers[right];
+
+    if (sum === result) {
+      return [numbers[left], numbers[right]];
+    } else if (sum < result) {
+      left++;
+    } else {
+      right--;
+    }
+  }
+  return [];
+}
+
+console.log(twoSum([2, 7, 11, 15], 9));
+console.log(twoSum([2, 7, 11, 15], 23));
